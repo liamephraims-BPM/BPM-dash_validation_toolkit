7eeede911b49791f970e34ba67be0116912f76e2
Liamephraims <46205886+Liamephraims@users.noreply.github.com>
2022-04-11 13:23:17 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='dash_validation_toolkit',
-    version='0.4.2',
+    version='0.5.2',
     author='Liam Ephraims',
     author_email='liam.ephraims@bigpicturemedical.com',
     description='Use driver functions and utility functions to run stage 1, 2 and 3 checks, can also run individual checks',
