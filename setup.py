b0912df1a81e649017f5888161e635d544245820
liamephraims-BPM <liam.ephraims@bigpicturemedical.com>
2022-04-20 11:25:22 +1000

    save

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='BPM-dash_validation_toolkit',
-    version='0.20.01',
+    name='BPM_dash_validation_toolkit',
+    version='0.21.01',
     author='Liam Ephraims',
     author_email='liam.ephraims@bigpicturemedical.com',
     description='Use driver functions and utility functions to run stage 1, 2 and 3 checks, can also run individual checks',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/liamephraims-BPM/BPM-dash_validation_toolkit',
+    url='https://github.com/liamephraims-BPM/BPM_dash_validation_toolkit',
     license='MIT',
-    packages=['BPM-dash_validation_toolkit'],
+    packages=['BPM_dash_validation_toolkit'],
     install_requires=['pyathena', 'pandas'] #, 
 )
