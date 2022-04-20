11188a1c06076ac5552033210973112dbbbc9f0e
liamephraims-BPM <liam.ephraims@bigpicturemedical.com>
2022-04-20 11:21:19 +1000

    save

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='BPM-dash_validation_toolkit',
-    version='0.19.01',
+    version='0.20.01',
     author='Liam Ephraims',
     author_email='liam.ephraims@bigpicturemedical.com',
     description='Use driver functions and utility functions to run stage 1, 2 and 3 checks, can also run individual checks',
