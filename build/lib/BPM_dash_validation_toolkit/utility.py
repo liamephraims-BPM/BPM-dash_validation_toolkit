622b14fc6a002b483aa3e987a919de83ba0a4b31
liamephraims-BPM <liam.ephraims@bigpicturemedical.com>
2022-04-20 12:33:05 +1000

    save

---
+++ build/lib/BPM_dash_validation_toolkit/utility.py
@@ -0,0 +1,50 @@
+# Creating client object class for creating client objects for capturing warnings and errors
+class client:
+    def __init__(self, client, regions, prod_databases):
+        self.client = client
+        self.regions = regions
+        self.prod_databases = prod_databases
+        self.failures = dict()
+
+# creating utility function for easily creating the client set-up
+def set_up_client(client_str, regions_list, prod_databases_list):
+    # Creating a client object for keeping track of client validation:
+    client_object = client(client_str, regions_list, prod_databases_list)
+    clients_dict = dict()
+    clients_dict[client_str] = client_object
+    return clients_dict
+
+# Function to be an import that will allow for sending validation output to analytics slack channel:
+import requests
+import json
+#send data to webhook
+def send_data(text, slack_webhook):
+    WEBHOOK = slack_webhook
+    data = {"text":text}
+    headers = {'content-type': 'application/json'}
+    res = requests.post(WEBHOOK, data=json.dumps(data), headers=headers)
+    print(res.text)
+    
+# Creating utility function for outputting all the client failures and warnings:
+def output_client_validation_results(clients, slack_webhook, send_to_slack=True):
+    """
+        Print out the warnings/errors from the validation, and if send to slack parameter is not false, then also send to analytics slack channel
+    """
+    output_string = ""
+    # For a particular client:
+    for bpmclient in clients:
+        output_string += "------------------------------------------------------------Beginning Failures Output for {}-----------------------------------------------------\n".format(bpmclient)
+        output_string += "For client {}, the failures are:\n".format(bpmclient)
+        for failed_tables in clients[bpmclient].failures:
+            for failure in clients[bpmclient].failures[failed_tables].failures:
+                output_string += clients[bpmclient].failures[failed_tables].failures[failure]
+                for child in clients[bpmclient].failures[failed_tables].dependencies:
+                    output_string +="WARNING: Check {} failed for {} - this may affect the downstream table {}\n".format(failure, failed_tables, child)
+        output_string += "------------------------------------------------------------End of Failures output for {}-----------------------------------------------------\n\n".format(bpmclient)
+    if send_to_slack == True:
+        # first print out
+        print(output_string)
+        # then send to slack channel: 
+        send_data(output_string, slack_webhook)
+    else:
+        print(output_string)
